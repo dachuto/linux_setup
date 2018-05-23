73019a5912e51a2375893ea7d34969173594938b
dachuto <dachuto@gazeta.pl>
2018-05-23 19:27:04 +0200

    more robust; do not know why

--- bash/bashrc_append.sh
+++ bash/bashrc_append.sh
@@ -45,7 +45,7 @@ PS1='\[${BOLD}\] \[${PS1_EXIT_COLOR}\]${PS1_EXIT_STATUS} \[${NICE_BLUE}\]${PS1_T
 PROMPT_COMMAND=__prompt_command
 PROMPT_COMMAND="${PROMPT_COMMAND}; history -a"
 
-export PATH="~/bin/:$PATH"
+export PATH="${HOME}/bin/:${PATH}"
 
 # easy setup for workspace launcher
 set_terminal_title() {
